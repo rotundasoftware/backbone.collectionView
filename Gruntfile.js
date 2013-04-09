4d7e5f1d5acd6230b64aec58e714174fd8161987
Colin Wren <colinwrenca@gmail.com>
2013-04-08 21:12:06 -0700

    Added grunt and package.json for compiling Sass

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'css',
+          debugInfo: true
+        }
+      }
+    },
+    watch: {
+      compass: {
+        files: ['sass/*'],
+        tasks: ['compass']
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', 'watch');
+
+};
