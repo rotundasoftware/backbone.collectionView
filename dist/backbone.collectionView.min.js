292e52fa15aab3852deaecaceb9f412d63cc9ba8
David Beck <david@rotundasoftware.com>
2014-07-21 16:05:05 -0700

    Update backbone.collectionView.min.js

--- dist/backbone.collectionView.min.js
+++ dist/backbone.collectionView.min.js
@@ -1,5 +1,5 @@
 /*!
-* Backbone.CollectionView, v0.9.5
+* Backbone.CollectionView, v0.10.1
 * Copyright (c)2013 Rotunda Software, LLC.
 * Distributed under MIT license
 * http://github.com/rotundasoftware/backbone-collection-view
