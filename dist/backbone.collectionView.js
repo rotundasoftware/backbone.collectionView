f374480698fbe7eee0c283514876b2f53551f276
David Beck <david@rotundasoftware.com>
2014-08-18 23:50:31 -0700

    run grunt to update zips etc.

--- dist/backbone.collectionView.js
+++ dist/backbone.collectionView.js
@@ -1,5 +1,5 @@
 /*!
-* Backbone.CollectionView, v0.10.1
+* Backbone.CollectionView, v0.11.0
 * Copyright (c)2013 Rotunda Software, LLC.
 * Distributed under MIT license
 * http://github.com/rotundasoftware/backbone-collection-view
