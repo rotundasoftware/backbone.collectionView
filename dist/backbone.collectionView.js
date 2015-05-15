f1c96ba3089c0343c7350e7028b9cd69d7654850
David Beck <david@rotundasoftware.com>
2015-05-15 12:14:52 -0700

    bump version number

--- dist/backbone.collectionView.js
+++ dist/backbone.collectionView.js
@@ -1,5 +1,5 @@
 /*!
-* Backbone.CollectionView, v0.11.6
+* Backbone.CollectionView, v0.11.7
 * Copyright (c)2013 Rotunda Software, LLC.
 * Distributed under MIT license
 * http://github.com/rotundasoftware/backbone-collection-view
