8f7e920a959ba6faef8c0af250e310508d1d6a83
David Beck <david@rotundasoftware.com>
2017-09-04 12:05:26 -0700

    1.3.4

--- dist/backbone.collectionView.js
+++ dist/backbone.collectionView.js
@@ -1,5 +1,5 @@
 /*!
-* Backbone.CollectionView, v1.3.3
+* Backbone.CollectionView, v1.3.4
 * Copyright (c)2013 Rotunda Software, LLC.
 * Distributed under MIT license
 * http://github.com/rotundasoftware/backbone-collection-view
